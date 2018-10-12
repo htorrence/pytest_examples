6105a1418aa6f180cb98f0ba5c2a549eee9c84ea
Hanna Torrence <hanna.torrence@gmail.com>
2018-10-12 12:51:44 -0500

    add test sections

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,14 +5,17 @@ from pytest_examples.functions_to_test import add_col
 
 
 def test_add_col_passes_with_fixtures(df, df_with_col_d):
+    # setup
     df = pd.DataFrame({
         'col_a': ['a', 'a', 'a'],
         'col_b': ['b', 'b', 'b'],
         'col_c': ['c', 'c', 'c'],
     })
 
+    # call function
     actual = add_col(df, 'col_d', 'd')
 
+    # set expectations
     expected = pd.DataFrame({
         'col_a': ['a', 'a', 'a'],
         'col_b': ['b', 'b', 'b'],
@@ -20,6 +23,7 @@ def test_add_col_passes_with_fixtures(df, df_with_col_d):
         'col_d': ['d', 'd', 'd'],
     })
 
+    # assertion
     pd.testing.assert_frame_equal(actual, expected)
 
 
